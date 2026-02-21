c2d1a72e875b7352eaf1437d02036f3441a5b358
Rjesh-kumar <rajeshkumarrout40gmail.com>
2026-02-21 13:07:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000', 
-    'https://job-flow-fe.vercel.app'  // ✅ No trailing slash
+    'https://jobflow-fe.vercel.app/'  // ✅ No trailing slash
   ], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
